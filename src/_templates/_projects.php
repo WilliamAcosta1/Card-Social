ae908038cff8975baf6ef3dd61ec04ed08f51d53
WilliamAcosta1 <williamacosta0014@gmail.com>
2026-01-08 00:36:53 +0100

    Actualiza la lista de proyectos eliminando entradas obsoletas y añadiendo Zumito Framework.

--- src/_templates/_projects.php
+++ src/_templates/_projects.php
@@ -2,59 +2,9 @@
 
   $projects = [
     [
-      "name" => "xelA",
-      "desc" => "The only bot your Discord would ever need, it's time to ditch Mee6 and Tickets",
-      "url" => "https://xela.dev"
-    ],
-    [
-      "name" => "discord.http",
-      "desc" => "Python library that handles interactions from Discord POST requests.",
-      "url" => "https://github.com/AlexFlipnote/discord.http"
-    ],
-    [
-      "name" => "Homepage++",
-      "desc" => "A website that gives you a clean homepage.",
-      "url" => "https://github.com/AlexFlipnote/homepage_plusplus"
-    ],
-    [
-      "name" => "template.py",
-      "desc" => "A standardized Python template configuration for my projects.",
-      "url" => "https://github.com/AlexFlipnote/template.py"
-    ],
-    [
-      "name" => "KawaiiBot",
-      "desc" => "A kawaii bot that makes your server more kawaii",
-      "url" => "https://kawaiibot.dev/"
-    ],
-    [
-      "name" => "PostgresLite",
-      "desc" => "Python SQLite combined with syntax compared to asyncpg progject",
-      "url" => "https://github.com/AlexFlipnote/PostgresLite"
-    ],
-    [
-      "name" => "dotenvplus",
-      "desc" => "Reads key-value pairs from a .env file and supports multiple values with dynamic interpolation.",
-      "url" => "https://github.com/AlexFlipnote/dotenvplus"
-    ],
-    [
-      "name" => "Modesta",
-      "desc" => "🎨 A clean CSS framework made to be dark, responsive and easy to build with.",
-      "url" => "https://github.com/AlexFlipnote/Modesta"
-    ],
-    [
-      "name" => "GiveMeBadge",
-      "desc" => "A simple bot script that makes you gain the 'Active Developer' badge on Discord",
-      "url" => "https://github.com/AlexFlipnote/GiveMeBadge"
-    ],
-    [
-      "name" => "neofetch-win",
-      "desc" => "neofetch, but for Windows",
-      "url" => "https://github.com/AlexFlipnote/neofetch-win"
-    ],
-    [
-      "name" => "diskspace",
-      "desc" => "Making it possible to use Linux df & du command on Windows",
-      "url" => "https://github.com/AlexFlipnote/diskspace"
+      "name" => "Zumito Framework",
+      "desc" => "Framework for creating a bot for discord easily and quickly.",
+      "url" => "https://github.com/ZumitoTeam/zumito-framework"
     ],
   ];
 
