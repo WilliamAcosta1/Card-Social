a110060dbba96c36fd37388d13c88337dd261928
WilliamAcosta1 <williamacosta0014@gmail.com>
2026-01-08 20:39:02 +0100

    Elimina archivos de documentación obsoletos sobre limpieza de unidades y creación de claves SSH.

--- src/docs/getting_started_with_scss/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $title = "Getting started with SCSS";
-  $description = "Learning the basics of SCSS";
-  $path = "/docs/getting_started_with_scss";
-  $keywords = "tutorial, start, scss, getting started, easy";
-  $date = new DateTime("2019-02-28");
-
-  require_once("./src/_templates/_markdown.php");
-?>
