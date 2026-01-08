4156c5ea7f8467326d03cb6074a5ed7d5f0b2573
WilliamAcosta1 <williamacosta0014@gmail.com>
2026-01-08 20:24:32 +0100

    Actualiza la lista de proyectos y corrige el estilo del botón de Spotify

--- src/_templates/_projects.php
+++ src/_templates/_projects.php
@@ -2,10 +2,30 @@
 
   $projects = [
     [
+      "name" => "Zumito Team",
+      "desc" => "Co-founder of Zumito Team.",
+      "url" => "https://github.com/ZumitoTeam"
+    ],
+    [
       "name" => "Zumito Framework",
       "desc" => "Framework for creating a bot for discord easily and quickly.",
       "url" => "https://github.com/ZumitoTeam/zumito-framework"
     ],
+    [
+      "name" => "Zumito Bot",
+      "desc" => "The bot that runs on the Zumito Team Discord server.",
+      "url" => "https://github.com/ZumitoTeam/zumito-bot"
+    ],
+    [
+      "name" => "Zumito Website",
+      "desc" => "The website that runs on the Zumito Team Discord server.",
+      "url" => "https://github.com/ZumitoTeam/zumito-website"
+    ],
+    [
+      "name" => "Zumito Discord Server",
+      "desc" => "The Zumito Team Discord server.",
+      "url" => "https://discord.gg/nbvy8qQyqD"
+    ]
   ];
 
 ?>
